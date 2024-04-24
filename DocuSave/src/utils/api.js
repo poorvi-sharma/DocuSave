d16199c2e9f0471b273eb84c9c9980f1f2b4c6f1
Pradhumn Chourey <pradhumn.rchourey@gmail.com>
2024-04-24 16:14:27 +0530

    Added fetchUserId func - fetch userId from AsyncStorage

--- DocuSave/src/utils/api.js
+++ DocuSave/src/utils/api.js
@@ -34,7 +34,7 @@ const signUp = async (user) => {
 };
 
 const uploadDocument = async (docName, docType, docUri) => {
-  const userId = "1";
+  const userId = await fetchUserId();
   console.log("in uploadDocument");
   const data = {
     docName: docName,
@@ -43,11 +43,6 @@ const uploadDocument = async (docName, docType, docUri) => {
     userId: userId
   }
   try {
-    // const documentData = new FormData();
-    // documentData.append("document", document);
-    // documentData.append("docType", docType);
-    // documentData.append("docName", docName);
-
     const response = await axios.post(`${BASE_URL}/upload`, data);
     console.log("Document uploaded");
     console.log("Upload response: ", response.data);
@@ -58,38 +53,23 @@ const uploadDocument = async (docName, docType, docUri) => {
 };
 
 const getDocumentList = async () => {
-  const response = await axios.get(`${BASE_URL}/documents/1`);
-  console.log(response.data[0]);
+  const userId = await fetchUserId();
+  const response = await axios.get(`${BASE_URL}/documents/${userId}`);
+  // console.log(response.data[0]);
   return response.data;
-  // return {
-  //   data: {
-  //     documents: [
-  //       { id: 1, name: "Document 41", type: "PDF" },
-  //       { id: 2, name: "Document 2", type: "Word" },
-  //       { id: 3, name: "Document 3", type: "Excel" },
-  //       { id: 4, name: "Document 1", type: "PDF" },
-  //       { id: 5, name: "Document 2", type: "Word" },
-  //       { id: 6, name: "Document 3", type: "Excel" },
-  //       { id: 7, name: "Document 1", type: "PDF" },
-  //       { id: 8, name: "Document 2", type: "Word" },
-  //       { id: 9, name: "Document 3", type: "Excel" },
-  //       { id: 10, name: "Document 1", type: "PDF" },
-  //       { id: 11, name: "Document 2", type: "Word" },
-  //       { id: 12, name: "Document 3", type: "Excel" },
-  //       { id: 13, name: "Document 1", type: "PDF" },
-  //       { id: 14, name: "Document 2", type: "Word" },
-  //       { id: 15, name: "Document 3", type: "Excel" },
-  //       { id: 16, name: "Document 1", type: "PDF" },
-  //       { id: 17, name: "Document 2", type: "Word" },
-  //       { id: 18, name: "Document 3", type: "Excel" },
-  //       { id: 19, name: "Document 1", type: "PDF" },
-  //       { id: 20, name: "Document 2", type: "Word" },
-  //       { id: 21, name: "Document 3", type: "Excel" },
-  //       // Add more sample documents as needed
-  //     ],
-  //   },
-  // };
+};
+
+const fetchUserId = async () => {
+  try {
+    const storedData = await AsyncStorage.getItem("user"); // Replace 'key' with the key you used while storing the data
+    if (storedData !== null) {
+      const dataObj = JSON.parse(storedData);
+      const userId = parseInt(dataObj.userId, 10);
+      return userId;
+    }
+  } catch (error) {
+    console.log('Error fetching data:', error);
+  }
 };
 
 export { getDocumentList, login, signUp, uploadDocument };
-
