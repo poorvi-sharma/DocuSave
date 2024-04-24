7b46db978e1b2534bb43ac6d44d65f5d6ec67c72
Pradhumn Chourey <pradhumn.rchourey@gmail.com>
2024-04-24 15:05:03 +0530

    FIxed home screen - view document list

--- DocuSave/src/utils/api.js
+++ DocuSave/src/utils/api.js
@@ -57,35 +57,38 @@ const uploadDocument = async (docName, docType, docUri) => {
   }
 };
 
-const getDocumentList = () => {
-  return {
-    data: {
-      documents: [
-        { id: 1, name: "Document 41", type: "PDF" },
-        { id: 2, name: "Document 2", type: "Word" },
-        { id: 3, name: "Document 3", type: "Excel" },
-        { id: 4, name: "Document 1", type: "PDF" },
-        { id: 5, name: "Document 2", type: "Word" },
-        { id: 6, name: "Document 3", type: "Excel" },
-        { id: 7, name: "Document 1", type: "PDF" },
-        { id: 8, name: "Document 2", type: "Word" },
-        { id: 9, name: "Document 3", type: "Excel" },
-        { id: 10, name: "Document 1", type: "PDF" },
-        { id: 11, name: "Document 2", type: "Word" },
-        { id: 12, name: "Document 3", type: "Excel" },
-        { id: 13, name: "Document 1", type: "PDF" },
-        { id: 14, name: "Document 2", type: "Word" },
-        { id: 15, name: "Document 3", type: "Excel" },
-        { id: 16, name: "Document 1", type: "PDF" },
-        { id: 17, name: "Document 2", type: "Word" },
-        { id: 18, name: "Document 3", type: "Excel" },
-        { id: 19, name: "Document 1", type: "PDF" },
-        { id: 20, name: "Document 2", type: "Word" },
-        { id: 21, name: "Document 3", type: "Excel" },
-        // Add more sample documents as needed
-      ],
-    },
-  };
+const getDocumentList = async () => {
+  const response = await axios.get(`${BASE_URL}/documents/1`);
+  console.log(response.data[0]);
+  return response.data;
+  // return {
+  //   data: {
+  //     documents: [
+  //       { id: 1, name: "Document 41", type: "PDF" },
+  //       { id: 2, name: "Document 2", type: "Word" },
+  //       { id: 3, name: "Document 3", type: "Excel" },
+  //       { id: 4, name: "Document 1", type: "PDF" },
+  //       { id: 5, name: "Document 2", type: "Word" },
+  //       { id: 6, name: "Document 3", type: "Excel" },
+  //       { id: 7, name: "Document 1", type: "PDF" },
+  //       { id: 8, name: "Document 2", type: "Word" },
+  //       { id: 9, name: "Document 3", type: "Excel" },
+  //       { id: 10, name: "Document 1", type: "PDF" },
+  //       { id: 11, name: "Document 2", type: "Word" },
+  //       { id: 12, name: "Document 3", type: "Excel" },
+  //       { id: 13, name: "Document 1", type: "PDF" },
+  //       { id: 14, name: "Document 2", type: "Word" },
+  //       { id: 15, name: "Document 3", type: "Excel" },
+  //       { id: 16, name: "Document 1", type: "PDF" },
+  //       { id: 17, name: "Document 2", type: "Word" },
+  //       { id: 18, name: "Document 3", type: "Excel" },
+  //       { id: 19, name: "Document 1", type: "PDF" },
+  //       { id: 20, name: "Document 2", type: "Word" },
+  //       { id: 21, name: "Document 3", type: "Excel" },
+  //       // Add more sample documents as needed
+  //     ],
+  //   },
+  // };
 };
 
 export { getDocumentList, login, signUp, uploadDocument };
